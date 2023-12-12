f2f7af1b310bf42c6c3e76d3b7cccef63f5c3c8b
siva <desinidisivagangadhar@gmail.com>
2023-12-12 11:35:38 +0530

    adding file

--- create.sh
+++ create.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dipatch")
-for i in "${NAMES[@]}"
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0feb1eadee760c918
+
+#here, mysql or mongodb instance_type should be t3.medium, for others it is t2.micro
+
+    for i in "${NAMES[@]}"
 do
-echo "NAME: $i"
+    if [[ $i == "mongodb" || $i = "mysql"]]
+then
+    INSTANCE_TYPE="t3.medium"
+else    
+    INSTANCE_TYPE="t2.micro"
+fi
 
+    echo "creating $i instance"
+
+
+
+
+aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE="" --security-group-ids sg-0feb1eadee760c918
+--tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value=$i}]"
 done
-
-
-#aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro --security-group-ids sg-0feb1eadee760c918 
