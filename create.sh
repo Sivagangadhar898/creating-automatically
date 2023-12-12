2f8d7b87ebad773d71528a7baf71329463765114
siva <desinidisivagangadhar@gmail.com>
2023-12-12 20:09:45 +0530

    adding file

--- create.sh
+++ create.sh
@@ -20,4 +20,15 @@ fi
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
+    {
+        
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "'$i.$DOMAIN_NAME'",
+                                    "Type": "A",
+                                    "TTL": 300,
+                                 "ResourceRecords": [{ "Value": "$IP_ADDRESS"}]
+                                 }}]
+                        }
 done
