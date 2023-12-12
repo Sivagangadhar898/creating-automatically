a68f51de4d30e6d274a675091c8c4ed31034aa9c
siva <desinidisivagangadhar@gmail.com>
2023-12-12 18:51:32 +0530

    adding file

--- create.sh
+++ create.sh
@@ -19,7 +19,6 @@ fi
 
     echo "creating $i instance"
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].
-    privateIpAddress')
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].privateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
 done
