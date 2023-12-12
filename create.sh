10884664a427375eab9507eed5aff9e3481c97a5
siva <desinidisivagangadhar@gmail.com>
2023-12-12 20:27:52 +0530

    adding file

--- create.sh
+++ create.sh
@@ -4,7 +4,7 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-0feb1eadee760c918
-
+DOMAIN_NAME=joiningindevops.online
 #here, mysql or mongodb instance_type should be t3.medium, for others it is t2.micro
 
     for i in "${NAMES[@]}"
@@ -20,15 +20,16 @@ fi
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
+    aws route53 change-resource-record-sets --hosted-zone-id Z05518102WFCFT3ITDVAC --change-batch '
     {
-        
-            "Changes": [{
+        "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
                                     "Name": "'$i.$DOMAIN_NAME'",
                                     "Type": "A",
                                     "TTL": 300,
-                                 "ResourceRecords": [{ "Value": "$IP_ADDRESS"}]
+                                 "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
                                  }}]
                         }
+                        '
 done
