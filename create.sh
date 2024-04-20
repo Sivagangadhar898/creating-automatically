6a194db9daed4847e73443ae367a0ef148fa4e32
siva <desinidisivagangadhar@gmail.com>
2024-04-20 11:46:46 +0530

    ansible

--- create.sh
+++ create.sh
@@ -2,8 +2,8 @@
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dipatch" "web")
 INSTANCE_TYPE=""
-IMAGE_ID=ami-03265a0778a880afb
-SECURITY_GROUP_ID=sg-0feb1eadee760c918
+IMAGE_ID=ami-0f3c7d07486cad139
+SECURITY_GROUP_ID=sg-0c1104897a46b8099
 DOMAIN_NAME=joiningindevops.online
 #here, mysql or mongodb instance_type should be t3.medium, for others it is t2.micro
 
@@ -20,7 +20,7 @@ fi
     echo "creating $i instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "created $i instance: $IP_ADDRESS"
-    aws route53 change-resource-record-sets --hosted-zone-id Z05518102WFCFT3ITDVAC --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id Z07158072WDZ3I52J42IG --change-batch '
     {
         "Changes": [{
             "Action": "CREATE",
